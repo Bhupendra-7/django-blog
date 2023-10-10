8d7b8dce3ff8ad4cb1148520bd339b75a11f7faa
Admin <admin@email.com>
2023-10-10 17:30:21 +0530

    posts by category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,7 +19,9 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('category/', include('blogs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
