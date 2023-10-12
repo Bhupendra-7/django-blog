e11abbb35d2e07ef3d34bc8dc3de295322637968
Admin <admin@email.com>
2023-10-12 13:22:04 +0530

    blogs and search features

--- blogs/models.py
+++ blogs/models.py
@@ -32,3 +32,25 @@ class Blog(models.Model):
 
 	def __str__(self):
 		return self.title
+
+
+class About(models.Model):
+	heading = models.CharField(max_length=50)
+	description = models.TextField(max_length=250)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name_plural = 'About'
+
+	def __str__(self):
+		return self.heading
+
+class SocialLink(models.Model):
+	platform = models.CharField(max_length=20)
+	link = models.URLField(max_length=100)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.platform
